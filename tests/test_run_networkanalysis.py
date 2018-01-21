0f6f3cc358d1d97b23f59b8b11e3d05531df48c9
ppxasjsm <antonia.mey@fu-berlin.de>
2018-01-21 13:56:22 +0000

    adding tests for driver script

--- tests/test_run_networkanalysis.py
+++ tests/test_run_networkanalysis.py
@@ -3,13 +3,48 @@ import pytest
 import os
 import sys
 
-def test_target_compound():
+def test_no_file_passed():
+    executable = os.path.join(os.getcwd(),'bin','run_networkanalysis.py')
+    assert(os.path.exists(executable)==True)
+    #cmd = 'python '+executable+' '+graph_file
+    cmd = [sys.executable, executable]
+    error_string = b'You must give at least one networkanalysis networkx compatible file'
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    assert(error_string in stderr)
+
+def test_no_target_compound():
     executable = os.path.join(os.getcwd(),'bin','run_networkanalysis.py')
     graph_file = os.path.join(os.getcwd(),'tests','io', 'graph.csv')
     assert(os.path.exists(executable)==True)
     assert(os.path.exists(graph_file)==True)
     #cmd = 'python '+executable+' '+graph_file
-    cmd = [sys.executable, executable]
-    error_string = 'you must give at least one networkanalysis networkx compatible file'
-    #with pytest.raises(IOError, message = error_string):
-    #    subprocess.check_output(cmd)
+    cmd = [sys.executable, executable, graph_file]
+    error_string = b'No target compound given, using the first compound in the node list:'
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    assert(error_string in stderr)
+
+#def test_wrong_target_compound():
+#    executable = os.path.join(os.getcwd(),'bin','run_networkanalysis.py')
+#    graph_file = os.path.join(os.getcwd(),'tests','io', 'graph.csv')
+#    assert(os.path.exists(executable)==True)
+#    assert(os.path.exists(graph_file)==True)
+    #cmd = 'python '+executable+' '+graph_file
+#    cmd = [sys.executable, executable, graph_file, " --target_compound FXR20" ]
+#    error_string = b'node FXR20 not in graph'
+#    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+#    stdout, stderr = p.communicate()
+#    print (stderr)
+    #assert(error_string in stderr)
+
+#def test_safe_data():
+#    executable = os.path.join(os.getcwd(),'bin','run_networkanalysis.py')
+#    graph_file = os.path.join(os.getcwd(),'tests','io', 'graph.csv')
+#    assert(os.path.exists(executable)==True)
+#    assert(os.path.exists(graph_file)==True)
+#    cmd = [sys.executable, executable, graph_file]
+#    error_string = b'No target compound given, using the first compound in the node list.'
+#    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+#    stdout, stderr = p.communicate()
+
